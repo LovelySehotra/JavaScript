99bd9dbbc25d1a37b8fee741368d22b56c09fad6
Lovely Sehotra <100295428+LovelySehotra@users.noreply.github.com>
2023-08-24 12:30:01 +0000

    dataTypes

--- 01basic/01basic.js
+++ 01basic/01basic.js
@@ -1,1 +1,14 @@
-console.log("lovely");
+// console.log("lovely");
+"use strict"; // treat all jS code  as newer version
+//  alert(3+3)  // we use nodejs not browser
+// primitive data type
+// number => 2 to power 53;
+//bigint
+// string=>""
+// null => standalone value
+//undefined=>
+// symbol
+
+//Object
+// console.log(typeof undefined) ;// undefined
+// console.log(typeof null) ;// object
